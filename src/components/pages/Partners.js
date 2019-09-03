9aff89562ec2ae3a72057ca863e8f8e7dc97ea3b
Bryan Phang Ying Xian <bryanpyx@spgroup.com.sg>
2019-09-03 12:48:29 +0800

    added partners

--- src/components/pages/Partners.js
+++ src/components/pages/Partners.js
@@ -3,10 +3,11 @@ import '../css/Partners.css';
 import Footer from '../global/Footer'
 import Rakuten from '../../images/partners_img/Rakuten.png'
 import RakutenRapidAPI from '../../images/partners_img/RakutenRapidAPI.png'
-// import Intel from '../../images/partners_img/intel_logo.png'
 import Redbull from '../../images/partners_img/Redbull_logo.png'
 import Sutdec from '../../images/partners_img/ec_logo.png'
 import Tribe from '../../images/partners_img/Tribe_logo.png'
+import Circles from '../../images/partners_img/circles_life.png'
+
 
 
 class Partners extends Component {
@@ -20,11 +21,11 @@ class Partners extends Component {
                     <img className="image entre_logo home" src={Sutdec} alt="SUTD Entrepreneurship centre" />
                 </div>
                 <div className="Gold">
-                {/* <img className="image" src={Intel} alt="Intel" /> */}
+                <h2>GOLD</h2>
+                    <img className="image home" src={Rakuten} alt="Rakuten" />
                 </div>
                 <div className="Sliver">
-                <h2>SILVER</h2>
-                    <img className="image home" src={Rakuten} alt="Rakuten" />
+                {/* <h2>SILVER</h2> */}
                 </div>
                 <div className="Bronze">
                 <h2>BRONZE</h2>
@@ -34,6 +35,8 @@ class Partners extends Component {
                 <div className="ComPartner">
                     <h2>COMMUNITY PARTNER</h2>
                     <img className="image home" src={Tribe} alt="Tribe" />
+                    <img className="image home" src={Circles} alt="Circles.life" />
+
                 </div>
             </div>
             <Footer /> 
