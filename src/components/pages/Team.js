caac10d80738aab86b36cc7015b8ee1e6077f399
Bryan Phang Ying Xian <bryanpyx@spgroup.com.sg>
2019-09-04 12:12:24 +0800

    added team

--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -1,11 +1,55 @@
 import React, { Component } from "react";
+import Footer from '../global/Footer';
 import '../css/Team.css';
+import Helena from '../../images/team_img/Helena.jpg'
+import Petra from '../../images/team_img/Petra.jpg'
+import Emir from '../../images/team_img/Emir.jpg'
+import Naomi from '../../images/team_img/Naomi.jpg'
+import ChangHsi from '../../images/team_img/ChangHsi.jpg'
+import Bryan from '../../images/team_img/Bryan.jpg'
+
+
 
 class Team extends Component {
   render() {
     return (
-        <div> 
-            ABOUT
+        <div className = "container"> 
+          <div className = "containerTeam">
+            <h1>Team</h1>
+            <div>
+              <div className='profile'>
+                <div>
+                  <img className="avatar" src={Bryan} alt="Bryan" />
+                  <p>Head of JunctionX Singapore</p>
+                </div>
+                <div>
+                  <img className="avatar" src={ChangHsi} alt="ChangHsi" />
+                  <p>Head of JunctionX Singapore</p>
+                </div>
+              </div>
+              <div className='profile'>
+                <div>
+                  <img className="avatar" src={Petra} alt="Petra" />
+                  <p>Marketing Strategist</p>
+                </div>
+                <div>
+                  <img className="avatar" src={Emir} alt="Emir" />
+                  <p>Marketing Designer</p>
+                </div>
+              </div>
+              <div className='profile'>
+                <div>
+                  <img className="avatar" src={Naomi} alt="Naomi" />
+                  <p>Apparel Designer</p>
+                </div>
+                <div>
+                  <img className="avatar" src={Helena} alt="Helena Hilander" />
+                  <p>Head of Participants</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
         </div>
     );
   }
