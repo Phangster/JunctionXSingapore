6367e1cf006fbe54d3da7c5ff605dead62056cda
Bryan Phang Ying Xian <bryanpyx@spgroup.com.sg>
2019-09-04 10:59:56 +0800

    added new partner

--- src/components/pages/Partners.js
+++ src/components/pages/Partners.js
@@ -7,6 +7,8 @@ import Redbull from '../../images/partners_img/Redbull_logo.png'
 import Sutdec from '../../images/partners_img/ec_logo.png'
 import Tribe from '../../images/partners_img/Tribe_logo.png'
 import Circles from '../../images/partners_img/circles_life.png'
+import StartupX from '../../images/partners_img/startupx1.jpeg'
+
 
 
 
@@ -18,25 +20,36 @@ class Partners extends Component {
             <h1>OUR PARNTERS</h1>
                 <div className="CoOrganising">
                 <h2>CO-ORGANISING</h2>
-                    <img className="image entre_logo home" src={Sutdec} alt="SUTD Entrepreneurship centre" />
+                    <div className="container-box">
+                        <img className="image entre_logo home" src={Sutdec} alt="SUTD Entrepreneurship centre" />
+                    </div>
                 </div>
                 <div className="Gold">
                 <h2>GOLD</h2>
-                    <img className="image home" src={Rakuten} alt="Rakuten" />
+                    <div className="container-box">
+                        <img className="image home" src={Rakuten} alt="Rakuten" />
+                    </div>
                 </div>
                 <div className="Sliver">
                 {/* <h2>SILVER</h2> */}
                 </div>
                 <div className="Bronze">
                 <h2>BRONZE</h2>
-                    <img className="image home" src={RakutenRapidAPI} alt="Rakuten Rapid API" />
-                    <img className="image home" src={Redbull} alt="Allswell" />
+                    <div className="container-box">
+                        <img className="image home" src={RakutenRapidAPI} alt="Rakuten Rapid API" />
+                        <img className="image home" src={Redbull} alt="Allswell" />
+                    </div>
                 </div>
                 <div className="ComPartner">
                     <h2>COMMUNITY PARTNER</h2>
-                    <img className="image home" src={Tribe} alt="Tribe" />
-                    <div className="boxing">
-                        <img className="image circles" src={Circles} alt="Circles.life" />
+                    <div className="container-box">
+                        <img className="image home" src={Tribe} alt="Tribe" />
+                        <div className="boxing">
+                            <img className="image circles" src={Circles} alt="Circles.life" />
+                        </div>
+                        <div className="boxing">
+                            <img className="image" src={StartupX} alt="StartUpX" />
+                        </div>
                     </div>
                 </div>
             </div>
