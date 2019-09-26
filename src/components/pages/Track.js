ce5d5d1ff91fa72ba3f655a9071dfbbd44035ccd
bryan phang <>
2019-09-26 10:31:39 +0800

    added prize

--- src/components/pages/Track.js
+++ src/components/pages/Track.js
@@ -46,7 +46,8 @@ class Track extends Component {
                                     <h3 className="textColor">1st</h3>
                                     <p>All team members gets a the *GOLDEN TICKET</p>
                                     <p>$250 worth of Rakuten RapidAPI credits</p>
-                                    <p className="smallWords">* An opportunity of the life time to fly to Helsinki to Hackjunction to participate in Europe's largest hackathon with 200 Euros reimbursement and free accomodation over the period of the event</p>
+                                    <p className="smallWords">* An opportunity of the life time to fly to Helsinki to Hackjunction to participate in </p>
+                                    <p className="smallWords">Europe's largest hackathon with 200 Euros reimbursement and free accomodation over the period of the event</p>
                                 </div>
                                 <div>
                                     <h3 className="textColor">2nd</h3>
@@ -55,6 +56,58 @@ class Track extends Component {
                                 <div>
                                     <h3 className="textColor">3rd</h3>
                                     <p>$100 worth of Rakuten RapidAPI credits</p>
+                                </div>
+                            </div>
+                            <div></div>
+                            <div className="grid-track">
+                                <div className = "grid-subPrice">
+                                    <h1>Rakuten Prizes</h1>
+                                    <div>
+                                        <h3 className="textColor">1st</h3>
+                                        <p>1x Oculus Headset worth $800</p>
+                                        <p>$300 Cash</p>
+                                    </div>
+                                    <div>
+                                        <h3 className="textColor">2nd</h3>
+                                        <p>$200 cash</p>
+                                    </div>
+                                    <div>
+                                        <h3 className="textColor">3rd</h3>
+                                        <p>$100 Cash</p>
+                                    </div>
+                                </div>
+                                <div className = "grid-subPrice">
+                                    <h1>Shopee Prizes</h1>
+                                    <div>
+                                        <h3 className="textColor">1st</h3>
+                                        <p>$300 cash</p>
+                                    </div>
+                                    <div>
+                                        <h3 className="textColor">2nd</h3>
+                                        <p>$200 cash</p>
+                                    </div>
+                                    <div>
+                                        <h3 className="textColor">3rd</h3>
+                                        <p>$100 Cash</p>
+                                    </div>
+                                </div>
+                                <div className = "grid-subPrice">
+                                    <h1>Kaspersky Prizes</h1>
+                                    <div>
+                                        <h3 className="textColor">1st</h3>
+                                        <p> 3x Mouse</p>
+                                        <p> 3x Fitness tracker</p>
+                                    </div>
+                                    <div>
+                                        <h3 className="textColor">2nd</h3>
+                                        <p> 3x Kaspersky Hoodies </p>
+                                        <p> 3x Caps </p>
+                                    </div>
+                                    <div>
+                                        <h3 className="textColor">3rd</h3>
+                                        <p> 3x Polo T-shirt </p>
+                                        <p> 3x Drawstring bag</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
