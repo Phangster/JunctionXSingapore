630aaf7ed836433a1a9cd7bf9ff4ad4d0437644d
Daryll Wong <daryll_wong@hotmail.com>
2020-04-11 23:51:17 +0800

    refactor directory

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import Nav from "./components/global/Nav";
 import Home from "./components/pages/home/Home.jsx";
-import Partners from "./components/pages/Partners";
+import Partners from "./components/pages/events/JxSingapore2019/Partners";
 import Registration from "./components/pages/Registration";
 import Track from "./components/pages/Track";
 import Schedule from "./components/pages/Schedule";
