e31d2cbe34754186e6e64b361c3115754db8907d
Daryll Wong <daryll_wong@hotmail.com>
2020-04-14 05:04:21 +0800

    finalise stats component

---
+++ src/components/pages/events/JxSingapore2019/JxSingapore2019Stats.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import './JxSingapore2019Stats.css'
+import styled from 'styled-components';
+import { PieChart, Pie, Sector, Cell, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+
+// PIECHART from recharts
+const data = [
+    {name: 'Singaporean', value: 60}, 
+    {name: 'Indonesian', value: 20},
+    {name: 'Filipino', value: 10}, 
+    {name: 'Algerian', value: 6}
+];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const RADIAN = Math.PI / 180;    
+
+const ParticipantsPieChart = ()=>{
+    return (
+        <div style={{padding: 5+ 'px'}}>
+
+        <PieChart width={800} height={400}  align="center">
+
+        <Pie
+          data={data} 
+          cx={400} 
+          cy={200} 
+          innerRadius={110}
+          outerRadius={140} 
+          fill="#8884d8"
+          paddingAngle={5}
+          label          
+        >
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+
+        <Legend align="center"/>
+        <Tooltip/>
+      </PieChart>
+      </div>
+    )
+}
+
+
+
+// INFO COMPONENT - comprises of a large top element and a smaller bottom element
+const TopBigLetter = styled.div`
+  font-size: 100px;
+  fontwight: 900;
+  text-align: center;
+  line-height: 100px;
+`
+
+const BottomSmallLetter = styled.div`
+  font-size: 20px;
+  fontwight: 200;
+  text-align: center;
+`
+
+const infoComponent = styled.div`
+  
+`
+const InfoComponent = ({topWord, bottomWord})=>{
+    return (
+    <infoComponent style={{padding: 20+ 'px'}}>
+        <TopBigLetter>{topWord}</TopBigLetter>
+        <BottomSmallLetter>{bottomWord}</BottomSmallLetter>
+    </infoComponent>
+    )
+}
+
+
+
+class JxSingapore2019Stats extends Component {
+  render() {
+    return (
+        <div class = "containerHiring"> 
+
+            <div className="flexbox">
+            <div className="flexbox-left">JuntionX Singapore 2019 Stats</div>
+              {/* <div style={{width:20+'px'}}></div> */}
+              <div className="flexbox-right">
+                <InfoComponent topWord="03" bottomWord="tracks"/>
+                <InfoComponent topWord="40+" bottomWord="projects"/>
+                <InfoComponent topWord="160+" bottomWord="participants"/>
+                <InfoComponent topWord="10+" bottomWord="partners"/>
+              </div>
+            </div>
+
+
+            <div style={{'padding': 50+'px'}}></div>
+
+
+            <div className="flexbox">
+            <div className="flexbox-left">Diverse group of Participants</div>
+              {/* <div style={{width:20+'px'}}></div> */}
+
+              {/* <div className="flexbox-right">
+                <ParticipantsPieChart/>
+              </div> */}
+              <ParticipantsPieChart/>
+            </div>
+
+        </div>
+    );
+  }
+}
+
+export default JxSingapore2019Stats;
