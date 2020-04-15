fa90b6705f63291b7ff3ace5cc196d9ac1a61e19
haziqmofe <55946335+haziqmofe@users.noreply.github.com>
2020-04-15 15:47:19 +0800

    Team Page

--- src/components/pages/home/OurStory.jsx
+++ src/components/pages/home/OurStory.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
+import Footer from '../../global/Footer';
+import CoverPhoto from '../../../images/junction_img/individual.jpg'
+import '../../css/OurStory.css'
 
 class OurStory extends Component {
   render() {
+
     return (
-        <div> 
-            OurStory
+      <div>
+        <div className="StoryPage">
+          <div className="wrapper1">
+            <img src={CoverPhoto} className="wrapper1"></img>
+            <div className="coverContainer">
+              <p className="headerLogoCommunity">This is JunctionX Singapore</p>
+              <div id="header2LogoCommunity">
+                A non-profit multi-disciplinary tech-community that is managed and run by Singapore tech-enthusiasts.
+                We come together in order to bring tech-enthusiasts an opportunity to showcase their talents
+                as well as to contribute back to the community. After all, it is intended to empower people to create with technology.
+              </div>
+            </div>
+          </div>
+
+          <div class="wrapper2">
+            <div class="row">
+              <div class="col s2 l4">
+                <h3 class="MiddleSec">An extraordinary group of individuals brought together by their tech interest.</h3>
+                <p class="MiddleSec-Comments">What makes us special? Join us to find out.</p>
+              </div>
+            </div>
+
+            <div class="Divider"></div>
+            <div class="row">
+              <div class="col s2 l4">
+                <h3 class="MiddleSec">How did we started out?</h3>
+                <p class="MiddleSec-Comments">Our first local hackathon in Singapore took place in 2019.
+                Through JunctionX Singapore, participants were able to challenge themselves as well
+                as being able to push themselves in order to solve the the problems.</p>
+              </div>
+            </div>
+          </div>
+          <Footer />
         </div>
+      </div>
     );
   }
 }
--- src/components/pages/home/Team.jsx
+++ src/components/pages/home/Team.jsx
@@ -2,23 +2,22 @@ import React, { Component } from "react";
 import Footer from '../../global/Footer';
 import '../../css/Team.css';
 
-
 //Import Team Pictures
 import Bryan from '../../../images/team_img/Bryan.jpg'
-import Saana from '../../../images/team_img/Saana.jpg'
 
 //Marketing Team
 import JunYuan from '../../../images/team_img/JunYuan.jpg'
 import Mel from '../../../images/team_img/Mel.jpg'
 import Sean from '../../../images/team_img/Sean.jpg'
+import Saana from '../../../images/team_img/Saana.jpg'
 
 //Partnership Team
 import Eloy from '../../../images/team_img/Eloy.jpg'
 import Adam from '../../../images/team_img/Adam.jpg'
-import Daryl from '../../../images/team_img/Daryl.jpg'
 
 //Web Developer
 import Haziq from '../../../images/team_img/Haziq.jpg'
+import Daryl from '../../../images/team_img/Daryl.jpg'
 
 class Team extends Component {
   render() {
@@ -36,78 +35,92 @@ class Team extends Component {
                 <h2>Head of JunctionX Singapore</h2>
                 <img className="avatar" src={Bryan} alt="Bryan" />
                 <p>Bryan Phang</p>
-                <a href="mailto:bryan.david@hackjunction.com" title="Eloy Choo">bryan.david@hackjunction.com</a>
+                <a className="email" href="mailto:bryan.david@hackjunction.com" title="Eloy Choo">bryan.david@hackjunction.com</a>
               </div>
             </div>
+
+            <div className="Divider"></div>
+
 
             <h2>Partnership Team</h2>
             <div className='profile'>
               <div>
-                <img className="avatar" src={Eloy} alt="Eloy" />
-                <p>Eloy Choo Hui</p>
-                <p className="position">Partnership</p>
-                <a href="mailto:eloy.choo.hui@hackjunction.com" title="Eloy Choo">eloy.choo.hui@hackjunction.com</a>
-              </div>
-
-              <div>
                 <img className="avatar" src={Daryl} alt="Daryl" />
                 <p>Daryll Wong</p>
-                <p className="position">Partnership</p>
-                <a href="mailto:daryll.wong@hackjunction.com" title="Daryll Wong">daryll.wong@hackjunction.com</a>
+                <p className="position">Head of Partnership</p>
+                <a className="email" href="mailto:daryll.wong@hackjunction.com" title="Daryll Wong">daryll.wong@hackjunction.com</a>
+              </div>
+            </div>
+
+            <div className='profile'>
+              <div>
+                <img className="avatar" src={Eloy} alt="Eloy" />
+                <p>Eloy Choo Hui</p>
+                <p className="position">Team Member</p>
               </div>
 
               <div>
                 <img className="avatar" src={Adam} alt="Adam" />
                 <p>Adam Proft</p>
-                <p className="position">Partnership</p>
-                <a href="mailto:adam.proft@hackjunction.com" title="Adam Proft">adam.proft@hackjunction.com</a>
+                <p className="position">Team Member</p>
               </div>
             </div>
+
+            <div className="Divider"></div>
 
             <h2>Marketing Team</h2>
             <div className='profile'>
               <div>
                 <img className="avatar" src={JunYuan} alt="JunYuan" />
                 <p>Teng Jun Yuan</p>
-                <p className="position">Marketing Designer</p>
-                <a href="mailto:teng.jun.yuan@hackjunction.com" title="Jun Yuan">teng.jun.yuan@hackjunction.com</a>
+                <p className="position">Head of Marketing</p>
+                <a className="email" href="mailto:teng.jun.yuan@hackjunction.com" title="Jun Yuan">teng.jun.yuan@hackjunction.com</a>
               </div>
+            </div>
 
+            <div className='profile'>
               <div>
                 <img className="avatar" src={Mel} alt="Melody" />
                 <p>Melody Leong</p>
                 <p className="position">Marketing Designer</p>
-                <a href="mailto:melody.leong@hackjunction.com" title="Melody Leong">melody.leong@hackjunction.com</a>
               </div>
 
               <div>
                 <img className="avatar" src={Sean} alt="Sean" />
                 <p>Sean Lew Teng Siong</p>
                 <p className="position">Marketing Designer</p>
-                <a href="mailto:sean@hackjunction.com" title="Sean lew">sean@hackjunction.com</a>
+              </div>
+
+              <div>
+                <img className="avatar" src={Saana} alt="Saana" />
+                <p>Saana</p>
+                <p className="position">Content Editor</p>
               </div>
             </div>
+
+            <div className="Divider"></div>
 
             <h2>Web Development Team</h2>
             <div className='profile'>
               <div>
                 <img className="avatar" src={Haziq} alt="Haziq" />
                 <p>Muhammad Haziq Mofe</p>
-                <p className="position">Web Developer</p>
-                <a href="mailto:muhammad.haziq@hackjunction.com" title="Haziq Mofe">muhammad.haziq@hackjunction.com</a>
+                <p className="position">Head of Web Development</p>
+                <a className="email" href="mailto:muhammad.haziq@hackjunction.com" title="Haziq Mofe">muhammad.haziq@hackjunction.com</a>
               </div>
+            </div>
 
+            <div className='profile'>
               <div>
                 <img className="avatar" src={Daryl} alt="Daryl" />
                 <p>Daryll Wong</p>
                 <p className="position">Web Developer</p>
-                <a href="mailto:daryll.wong@hackjunction.com" title="Daryl Wong">daryll.wong@hackjunction.com</a>
               </div>
             </div>
           </div>
         </div>
         <Footer />
-      </div>
+      </div >
     );
   }
 }
