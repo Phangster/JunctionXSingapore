c098a713f4dc0aabe6063fe5c9263d37740ff726
saanakuu <saanakuu.u@gmail.com>
2020-04-20 13:04:34 +0800

    Language check JunctionXAsia2020

--- src/components/pages/events/JxAsia2020/JxAsia2020.jsx
+++ src/components/pages/events/JxAsia2020/JxAsia2020.jsx
@@ -43,7 +43,7 @@ class JxSingapore2019 extends Component {
               {/* <p className = "headerLogo">JUNCTIONX ASIA</p> */}
               <img src={AsiaLogo} className="headerLogoImg" ></img>
               <p style={{ fontSize: 30 + "px" }}>HACK THE FUTURE</p>
-              <div style={{ fontSize: 25 + "px" }} className="textTwo color">4. - 7.6.20</div>
+              <div style={{ fontSize: 25 + "px" }} className="textTwo color">4.-7.6.2020</div>
             </div>
           </div>
           <div className="wrapper2 wrapper">
@@ -54,15 +54,12 @@ class JxSingapore2019 extends Component {
             </div>
               {/* <div style={{width:20+'px'}}></div> */}
             <div className="wrapperTwo-flexbox1-right">
-                <div>
-                JunctionX Asia is an online hackathon targeted at solving problems we are facing during this ongoing pandemic. Bringing together developers, designers and other tech-minded individuals to create projects and solve intriguing challenges.
-                </div> <br/>
-                <div>
-                We will be collaborating with industry partners to come up with real life problem statements for the participants. Come join us! Registration opening in May and more details will be released soon!
-                </div>
-                <div>
-                Stay tuned:)
-                </div>
+              <div>
+              JunctionX Asia is an online hackathon targeted at solving problems we are facing during this ongoing pandemic. Bringing together developers, designers and other tech-minded individuals to create projects and solve intriguing challenges.
+              </div> <br/>
+              <div>
+              We will be collaborating with industry partners to come up with real-life problem statements for the participants. Come join us! Registration will open in May and more details will be released soon - stay tuned!
+              </div>
             </div>
             </div>
             <br/><br/><br/>
