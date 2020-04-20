d1da30d2e3f35d06ed0f6f4f2095a4e108c714b0
saanakuu <saanakuu.u@gmail.com>
2020-04-20 14:54:09 +0800

    Language check ForVolunteers

--- src/components/pages/community/ForVolunteers.jsx
+++ src/components/pages/community/ForVolunteers.jsx
@@ -23,9 +23,10 @@ class ForVolunteers extends Component {
         <div className = "wrapper1">
             <img src={ForVolunteersCoverImg} className="coverPhotoTag"></img>
           <div className = "coverContainer">
-            <p className = "headerLogoCommunity">Join the JunctionX Singapore Family</p>
+            <p className = "headerLogoCommunity">Join the JunctionX Singapore family</p>
             <div id="header2LogoCommunity">
-            We are JunctionX Singapore, part of JunctionX – a volunteer-lead community effort around the world. Our headquarters is located at Startup Sauna in Finland but the Junction team as a whole spreads out all around the globe. The thing that connects us all? A passion for empowering people to create with technology. Join us!
+            We are JunctionX Singapore, part of JunctionX – a volunteer-lead community effort around the world. Our headquarters is located at Startup Sauna in Finland but the Junction team as a whole spreads out all around the globe. The thing that connects us all? A passion for empowering people to create with technology. 
+            <br/>Join us!
             </div>
             {/* <div style={{fontSize: 15 +"px"}} className = "textTwo color">
             We organize hackathons, speaker events, coding workshops and other tech events around the year. We have something for every skill set and interest: take on new challenges, explore, and gain new experiences in Junction events!
@@ -39,7 +40,7 @@ class ForVolunteers extends Component {
           {/* <div style={{'margin-bottom': 60+'px'}}></div> */}
           <div className="flexbox1 flexbox">
             <div className="wrapperTwo-flexbox1-left">
-                <div>A Diverse Team</div>
+                <div>A diverse team</div>
             </div>
               {/* <div style={{width:20+'px'}}></div> */}
             <div className="wrapperTwo-flexbox1-right">
@@ -49,8 +50,8 @@ class ForVolunteers extends Component {
                 <div>
                 Junction events are organized throughout the year, including JunctionX hackathons around the world. 
                 The winning team for JunctionX Singapore will get the opportunity to go to the annual main event in Finland: next up, Junction 2020!
-                If you’re into tech at all, it is the place to be – a full weekend of hacking with around other tech heads & hustlers in an awesome venue and atmosphere like no other.
-                An experience you do not want to miss out on. Stay tuned for more information!
+                If you’re into tech at all, it is the place to be – a full weekend of hacking with around other tech enthusiasts & hustlers in an awesome venue and atmosphere like no other.
+                An experience you don't want to miss. Stay tuned for more information!
                 </div>
             </div>
           </div>
@@ -67,7 +68,7 @@ class ForVolunteers extends Component {
                 "It will be a rewarding and fulfilling journey" - Santiana
                 </div> <br/>
                 <div>
-                "Never got to join hackathons but it is a eye-opening to me. Glad that I became a volunteer to see what its like" - Joordin
+                "Never got to join hackathons but it is a eye-opening to me. Glad that I became a volunteer to see what it's like" - Joordin
                 </div><br/>
                 <div>
                 "Quite a good atmosphere! Love it" - Ben
@@ -82,12 +83,12 @@ class ForVolunteers extends Component {
         <div className = "wrapper3 wrapper">
 
         <div className="flexbox4 flexbox">
-            <div className="wrapperTwo-flexbox1-left">Interested? Come talk to us</div>
+            <div className="wrapperTwo-flexbox1-left">Interested? Let's have a chat!</div>
               {/* <div style={{width:20+'px'}}></div> */}
             <div className="wrapperTwo-flexbox1-right">
-            <span>Feel free to contact Bryan, Head of JunctionX Singapore </span>
+            <span>Feel free to contact Bryan, Head of JunctionX Singapore: </span>
                 <a href="mailto:bryan.david@hackjunction.com?Subject=CollaborationForJunctionXSingapore" target="_top">bryan.david@hackjunction.com</a>
-                <br/><span> and let's have a chat:)</span>
+                <br/>
                 
             </div>
         </div>
