1ab1fb0575a340ec415f43d7678c2ac375bf1d6b
Daryll Wong <daryll_wong@hotmail.com>
2020-05-03 20:30:58 +0800

    npm updates?

---
+++ build/precache-manifest.6f56bced19a82d93d35c52e409b522b6.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559dd81500513d6adfb0ef6a1e85b15a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a17c60ba066d4b77d2",
+    "url": "/static/css/main.c6b5de4d.chunk.css"
+  },
+  {
+    "revision": "559f18cc97ca2df35fed",
+    "url": "/static/js/2.3ead6d4c.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.3ead6d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a17c60ba066d4b77d2",
+    "url": "/static/js/main.33e8d3a9.chunk.js"
+  },
+  {
+    "revision": "0fc17203e3dcd2cf965b",
+    "url": "/static/js/runtime-main.7bfe56a6.js"
+  },
+  {
+    "revision": "3a24182b5201adc9347851bb5f6f444c",
+    "url": "/static/media/Adam.3a24182b.jpg"
+  },
+  {
+    "revision": "b28178bf61a22ce097c5b150b0dc95bc",
+    "url": "/static/media/Bryan.b28178bf.jpg"
+  },
+  {
+    "revision": "56ef2bd3cc5f7a212989602e3ae08ec2",
+    "url": "/static/media/Eloy.56ef2bd3.jpg"
+  },
+  {
+    "revision": "7ba797852ebf1eef76b157d5f498b43f",
+    "url": "/static/media/ForParticipantsCoverImg.7ba79785.jpg"
+  },
+  {
+    "revision": "34e34aec357731e456c35efbf0e341b5",
+    "url": "/static/media/ForPartnersCoverImg.34e34aec.jpg"
+  },
+  {
+    "revision": "637337190ca46f2af3c747b80b7d322a",
+    "url": "/static/media/ForVolunteersCoverImg.63733719.jpg"
+  },
+  {
+    "revision": "a96f1954a5e2852666c449ce1b3f4834",
+    "url": "/static/media/Haziq.a96f1954.jpg"
+  },
+  {
+    "revision": "e6b5b4fc9e4d462af7eba31e9321e60e",
+    "url": "/static/media/Jeff_Guo_Qing.e6b5b4fc.png"
+  },
+  {
+    "revision": "4cd4eb08e38c0b5d212a32761d239126",
+    "url": "/static/media/JunYuan.4cd4eb08.jpg"
+  },
+  {
+    "revision": "830444bb265d77f3d904510b21ef589a",
+    "url": "/static/media/JunctionX Singapore logo_white_twoline.830444bb.png"
+  },
+  {
+    "revision": "960c8190530043869bf9ae4ac104aeb6",
+    "url": "/static/media/JunctionXAsia_Poster.960c8190.jpg"
+  },
+  {
+    "revision": "7e11d547ddce293bf1d47cc1da730db6",
+    "url": "/static/media/JunctionXSGtable.7e11d547.jpg"
+  },
+  {
+    "revision": "fb0d34896d94c1929de6d442c990011b",
+    "url": "/static/media/JunctionX_2020_intro.fb0d3489.jpg"
+  },
+  {
+    "revision": "7ae57dd00df696d0b8c20adc631d533a",
+    "url": "/static/media/JunctionX_Singapore_logo_black_twoline.7ae57dd0.png"
+  },
+  {
+    "revision": "2aad815e4eb2e7026ad94e75a7846688",
+    "url": "/static/media/JunctionX_logos-wordmark-white.2aad815e.svg"
+  },
+  {
+    "revision": "154b43c306228b2aa3d1edca7d476c99",
+    "url": "/static/media/JunctionX_logos_white.154b43c3.png"
+  },
+  {
+    "revision": "7ae57dd00df696d0b8c20adc631d533a",
+    "url": "/static/media/JunctionX_nav.7ae57dd0.png"
+  },
+  {
+    "revision": "9855acc75751e96369cf187b154bf09c",
+    "url": "/static/media/JxAsiaCalendarImg.9855acc7.png"
+  },
+  {
+    "revision": "d3854b5c74be59528089eef6f5d512d0",
+    "url": "/static/media/JxAsiaCoverImg.d3854b5c.png"
+  },
+  {
+    "revision": "dcd47e405a812b2f2b514c95acc9dde1",
+    "url": "/static/media/Liang_Jingxi.dcd47e40.jpg"
+  },
+  {
+    "revision": "fa57152a78f73bea35db9977f0f43752",
+    "url": "/static/media/OurPartnersCoverImg.fa57152a.jpg"
+  },
+  {
+    "revision": "56e51226f2977dcf255a7913b4b80264",
+    "url": "/static/media/Phan_Huy_Nguyen.56e51226.jpg"
+  },
+  {
+    "revision": "94c02e24fcb45e0152b1dadcd9b0a216",
+    "url": "/static/media/Rakuten.94c02e24.png"
+  },
+  {
+    "revision": "905e3a0832ea593d66b6e0037185aada",
+    "url": "/static/media/Reassemble.905e3a08.jpeg"
+  },
+  {
+    "revision": "ea11116b2819ad36793da913a2e3e6f7",
+    "url": "/static/media/Redbull_logo.ea11116b.png"
+  },
+  {
+    "revision": "36e5087f2481743db7d41959f9973d31",
+    "url": "/static/media/Saana.36e5087f.jpg"
+  },
+  {
+    "revision": "ae52848ba285c6991b013f9b2eef6d35",
+    "url": "/static/media/Salesforce.ae52848b.png"
+  },
+  {
+    "revision": "e73f0b576a38956866c695abfeb13ae7",
+    "url": "/static/media/Sean.e73f0b57.jpg"
+  },
+  {
+    "revision": "12eab1d61868ca5cedc84c0ae7b98f3b",
+    "url": "/static/media/Shopee.12eab1d6.png"
+  },
+  {
+    "revision": "e931c9914b686a57172a3b103846b452",
+    "url": "/static/media/ambidextr.e931c991.png"
+  },
+  {
+    "revision": "a0d6d6b8d5605947e87ace74e0b25878",
+    "url": "/static/media/arvr.a0d6d6b8.jpg"
+  },
+  {
+    "revision": "de9e2f178349957f09ac39e39c8d6f04",
+    "url": "/static/media/calendarBackgroundPhoto.de9e2f17.jpg"
+  },
+  {
+    "revision": "08b1ada5f0af9ed797e445070542b102",
+    "url": "/static/media/circles_life.08b1ada5.png"
+  },
+  {
+    "revision": "5c8f2bcfc9e884531d4ef64953ce4f10",
+    "url": "/static/media/ec_logo.5c8f2bcf.png"
+  },
+  {
+    "revision": "054cc20027fb4a025f335f1fb5f6b8c8",
+    "url": "/static/media/eric_chang.054cc200.jpg"
+  },
+  {
+    "revision": "58c997d2c10a22c45708d27be083a67d",
+    "url": "/static/media/individual.58c997d2.jpg"
+  },
+  {
+    "revision": "48ddf12f6424c644aa5273aee0a422bc",
+    "url": "/static/media/intro.48ddf12f.mp4"
+  },
+  {
+    "revision": "667ddd1329a0c45941933bb20b6700d9",
+    "url": "/static/media/jackie.667ddd13.JPG"
+  },
+  {
+    "revision": "39e857b133a0a9e42dd60deff4eef8be",
+    "url": "/static/media/jed.39e857b1.jpeg"
+  },
+  {
+    "revision": "097a0eac75d45d5c0e606eb750df576e",
+    "url": "/static/media/junctionx_2020_collab.097a0eac.jpg"
+  },
+  {
+    "revision": "87631293e5ae61e70075ef1c5f7dfd87",
+    "url": "/static/media/junctionx_asia_white_oneline.87631293.png"
+  },
+  {
+    "revision": "be091edb4fb6bb82cd1e21bdda9b13df",
+    "url": "/static/media/junctionx_asia_white_twoline.be091edb.png"
+  },
+  {
+    "revision": "b3439e9a7db85fe2055cdfb95f6567c4",
+    "url": "/static/media/junctionx_globe_black.b3439e9a.svg"
+  },
+  {
+    "revision": "79230066bf33210a4d30388ba37250fb",
+    "url": "/static/media/junctionx_globe_white.79230066.svg"
+  },
+  {
+    "revision": "f9bcbe8275e069f241b5c28438e14083",
+    "url": "/static/media/junctionx_globe_white.f9bcbe82.png"
+  },
+  {
+    "revision": "217bee0020278926ee103ebaf94a9687",
+    "url": "/static/media/kaspersky.217bee00.png"
+  },
+  {
+    "revision": "8eb2f95a29698662a97ce26304b6a728",
+    "url": "/static/media/kaspersky_track.8eb2f95a.png"
+  },
+  {
+    "revision": "a512bebf910123a15816f58462812ee8",
+    "url": "/static/media/main_coverphoto.a512bebf.jpg"
+  },
+  {
+    "revision": "9e9c21a805b631f352c31cd4842630f8",
+    "url": "/static/media/marie_wong.9e9c21a8.jpeg"
+  },
+  {
+    "revision": "07093e8b0aa977bf7bbc14d9a3a8b0a4",
+    "url": "/static/media/mark_lven_palejaro.07093e8b.JPG"
+  },
+  {
+    "revision": "c134f5b092724ca69ed5282f1d1562b0",
+    "url": "/static/media/onlineWebinarImg.c134f5b0.jpg"
+  },
+  {
+    "revision": "376699cbff37c7099b0fe1066312383a",
+    "url": "/static/media/satoshi.376699cb.jpg"
+  },
+  {
+    "revision": "460678f34f2683d452b82545d471eb3e",
+    "url": "/static/media/security.460678f3.jpg"
+  },
+  {
+    "revision": "eb36b176b6beefbf8c219ea5a6616cd7",
+    "url": "/static/media/smartcommerce.eb36b176.jpg"
+  },
+  {
+    "revision": "4f8c86791eb14fb0638a10e5d31168e8",
+    "url": "/static/media/startupx1.4f8c8679.jpeg"
+  },
+  {
+    "revision": "af735b9691a01161e8dcb2aaddfef819",
+    "url": "/static/media/team.af735b96.jpg"
+  },
+  {
+    "revision": "89236235c93c9e698e0118d504052d0a",
+    "url": "/static/media/yash_datta.89236235.jpeg"
+  }
+]);
